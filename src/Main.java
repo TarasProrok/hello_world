03eb3804fb5507fc834e447f5085bfc9f7164cbf
TarasProrok <170684@ukr.net>
2022-11-09 22:38:14 +0200

    added git comands in comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 class HelloWorld {
+    // to track files by git -> git add "filename" or git add -u or git add .
+    // to make commit -> git commit -m "your message"
+    // to see commit history -> git log
+    // to go to other commit -> git checkout "commit hash"
+    // to create new branch -> get checkout -b "branch name"
+    // to switch to exiting branch -> get checkout "branch name"
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I`m doing it!");
