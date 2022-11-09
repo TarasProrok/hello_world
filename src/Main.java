ad788dfb7648e0c3d4adb4545c183c6900e25dcb
TarasProrok <170684@ukr.net>
2022-11-09 20:10:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-public class Main {
-
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("This is our second commit!");
+        System.out.println("Hello World!");
+        System.out.println("I`m doing it!");
     }
 }
