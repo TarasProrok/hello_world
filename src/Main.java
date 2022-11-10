d598095b848b580afe65f404070078028b96fca9
TarasProrok <170684@ukr.net>
2022-11-10 13:30:04 +0200

    added git comands in comments

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class HelloWorld {
     // to track files by git -> git add "filename" or git add -u or git add .
     // to make commit -> git commit -m "your message"
@@ -8,5 +10,19 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I`m doing it!");
+        int[] arrSource = {1, 2, 3, 4, 5};
+        int[] arrTarget = Arrays.copyOf(arrSource, arrSource.length);
+        String[] names = {"Taras", "Dima"};
+        arrSource[0] = 100;
+        System.out.println(arrTarget[0]); // отримаємо 1
+        System.out.println(arrSource[0]);
+        System.out.println(Arrays.toString(names));
+        String skyColor = "black";
+
+        if (skyColor.equals("blue")) {
+            System.out.println("На вулиці день");
+        } else {
+            System.out.println("На вулиці ніч");
+        }
     }
 }
