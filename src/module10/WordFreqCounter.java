3a12df8674e60840b7745c28d9911c9d71203e82
TarasProrok <170684@ukr.net>
2022-12-25 18:30:43 +0200

    Task3: WordFreqCounter

--- src/module10/WordFreqCounter.java
+++ src/module10/WordFreqCounter.java
@@ -3,24 +3,45 @@ import java.io.*;
 import java.util.*;
 
 public class WordFreqCounter {
+    public static Map<String, Integer> sortByValue(Map<String, Integer> hashMap) { //function to sort
+        // creating list of entries in HashMap
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<>(hashMap.entrySet());
+        // sorting list
+        list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
+        // putting data from sorting list to hashmap
+        HashMap<String, Integer> temp = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
     public static void main(String[] args) throws Exception {
-        Map<String, Integer> hashMap;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        // creating reader of file
         try (BufferedReader br = new BufferedReader
                 (new FileReader("/Users/mac/Desktop/Java/HelloWorld/src/module10/words.txt"))) {
             String[] words;
-            hashMap = new HashMap<>();
-
+        // reading line of file
             String line = br.readLine();
+            // splitting line on words and putting them in array
             while (line != null) {
                 words = line.split(" ");
+                // putting array in hashmap
                 for (String word : words) {
                     hashMap.merge(word, 1, Integer::sum);
                 }
                 line = br.readLine();
             }
         }
-            System.out.println(hashMap);
+        Map<String, Integer> hm1 = sortByValue(hashMap);
+        // printing hashmap
+        for (Map.Entry<String, Integer> en : hm1.entrySet()) {
+            System.out.println(en.getKey() +
+                    " - " + en.getValue());
         }
     }
+}
 
 
